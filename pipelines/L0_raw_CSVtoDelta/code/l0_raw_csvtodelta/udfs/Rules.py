b9b0333d44d2cad4446bba4f588faaf2ea9b3e5f
natetongwong@gmail.com <natetongwong@gmail.com>
2024-05-23 01:27:14 +0000

    Updates multiple datasets and pipelines    - Modifies L0_raw_loan_purpose, L0_raw_product_system, L1_raw_origination_system, csv_origination_system, csv_loan_purpose, csv_product_system datasets    - Adds L1_bronze_finance_residential_mortgages dataset    - Modifies L0_raw_CSVtoDelta, L1_Bronze_ExactAndInitialEnrichments pipelines    - Modifies L0_raw_rule_ID_mapping dataset

--- pipelines/L0_raw_CSVtoDelta/code/l0_raw_csvtodelta/udfs/Rules.py
+++ pipelines/L0_raw_CSVtoDelta/code/l0_raw_csvtodelta/udfs/Rules.py
@@ -3,17 +3,17 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
 def Override_Housing_Purpose(
-        Secnd_Purps_Type_Lbl: Column=col("Secnd_Purps_Type_Lbl"), 
-        Origination_system: Column=col("Origination_system"), 
-        Housing_Purpose: Column=col("Housing_Purpose")
+        secnd_purps_type_lbl: Column=col("Secnd_Purps_Type_Lbl"), 
+        origination_system: Column=col("Origination_system"), 
+        housing_purpose: Column=col("Housing_Purpose")
 ):
     return when(
           (
             (
-              (Secnd_Purps_Type_Lbl == lit(231))
-              & (Origination_system == lit("MP-001"))
+              (secnd_purps_type_lbl == lit(231))
+              & (origination_system == lit("MP-001"))
             )
-            & (Housing_Purpose == lit("OO"))
+            & (housing_purpose == lit("OO"))
           ),
           lit("IPL")
         )\
@@ -21,11 +21,11 @@ def Override_Housing_Purpose(
         .alias("EFS_Housing_Purpose")
 
 def Override_EFS_Residual_Term_Rule_ID(
-        Residual_years: Column=col("Residual_years"), 
-        Maturity_Date: Column=col("Maturity_Date")
+        residual_years: Column=col("Residual_years"), 
+        maturity_date: Column=col("Maturity_Date")
 ):
-    return when(((Residual_years > lit(0)) & (Residual_years <= lit(1))), lit(5))\
-        .when((Residual_years > lit(1)), lit(6))\
-        .when((Maturity_Date == lit("")), lit(7))\
+    return when(((residual_years > lit(0)) & (residual_years <= lit(1))), lit(5))\
+        .when((residual_years > lit(1)), lit(6))\
+        .when((maturity_date == lit("")), lit(7))\
         .otherwise(lit(8))\
         .alias("EFS_Residual_Term_Rule_ID")
--- pipelines/L1_Bronze_ExactAndInitialEnrichments/code/l1_bronze_exactandinitialenrichments/graph/add_efs_rule_id.py
+++ pipelines/L1_Bronze_ExactAndInitialEnrichments/code/l1_bronze_exactandinitialenrichments/graph/add_efs_rule_id.py
@@ -6,5 +6,5 @@ from prophecy.libs import typed_lit
 from l1_bronze_exactandinitialenrichments.config.ConfigStore import *
 from l1_bronze_exactandinitialenrichments.udfs import *
 
-def add_efs_rule_id(spark: SparkSession, reformatted_account_details: DataFrame) -> DataFrame:
-    return reformatted_account_details.select(expr("*"), Override_EFS_Residual_Term_Rule_ID())
+def add_efs_rule_id(spark: SparkSession, in0: DataFrame) -> DataFrame:
+    pass
