84c716ee52988ed7539f1d8a5c96afe3f7adc373
natetongwong@gmail.com <natetongwong@gmail.com>
2024-06-19 08:39:56 +0000

    Reverted to DEMO START 3.0, commit hash : 186c56a86b9e12aabdc1b558906163446a206f6d

--- pipelines/L1_Bronze_ExactAndInitialEnrichments/code/l1_bronze_exactandinitialenrichments/udfs/Rules.py
+++ pipelines/L1_Bronze_ExactAndInitialEnrichments/code/l1_bronze_exactandinitialenrichments/udfs/Rules.py
@@ -1,31 +1,3 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-
-def Override_Housing_Purpose(
-        Secnd_Purps_Type_Lbl: Column=col("Secnd_Purps_Type_Lbl"), 
-        Origination_system: Column=col("Origination_system"), 
-        Housing_Purpose: Column=col("Housing_Purpose")
-):
-    return when(
-          (
-            (
-              (Secnd_Purps_Type_Lbl == lit(231))
-              & (Origination_system == lit("MP-001"))
-            )
-            & (Housing_Purpose == lit("OO"))
-          ),
-          lit("IPL")
-        )\
-        .otherwise(col("housing_purpose"))\
-        .alias("EFS_Housing_Purpose")
-
-def Override_EFS_Residual_Term_Rule_ID(
-        Residual_years: Column=col("Residual_years"), 
-        Maturity_Date: Column=col("Maturity_Date")
-):
-    return when(((Residual_years > lit(0)) & (Residual_years <= lit(1))), lit(5))\
-        .when((Residual_years > lit(1)), lit(6))\
-        .when((Maturity_Date == lit("")), lit(7))\
-        .otherwise(lit(8))\
-        .alias("EFS_Residual_Term_Rule_ID")
